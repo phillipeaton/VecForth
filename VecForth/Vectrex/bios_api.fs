efebba1851527ab84149d94f64445c849465f4cb
Phillip Eaton <44277038+phillipeaton@users.noreply.github.com>
2020-03-29 00:08:00 +0100

    BIOS interface file and test programs update.
    - Big simplification on test programs.
    - Added all BIOS routines as CODE words, in categories.

--- VecForth/Vectrex/bios_api.fs
+++ VecForth/Vectrex/bios_api.fs
@@ -2,45 +2,12 @@
 
 HEX
 
-\ \\\\\\\\\\ NOT TESTED
-
-\ CODE DPR!   B DPR TFR,   6 # ( D) PULS, NEXT ;C \ DPR -- ;
-
-CODE _Intensity_a \ n -- ;
-    8 # ( DP) PSHU,           \ -- TOS ; Save DP
-    D0 # LDX,   X DPR TFR,    \ -- TOS ; DP to D0
-    Intensity_a JSR,
-    6 # ( D) PULS,            \ -- ; Drop TOS
-    8 # ( DP) PULU,           \ -- ; Restore DP
-    NEXT ;C
-
-CODE _Dot_List \ dl_addr -- ;
-    8 # ( DP) PSHU,           \ -- dl_addr ; Save DP
-    D0 # LDX,   X DPR TFR,    \ -- dl_addr ; DP to D0
-    D X TFR,                  \ -- dl_addr ; Load X with address of dot list
-    Dot_here JSR,             \ Call Vectrex BIOS subroutine
-    6 # ( D) PULS,            \ -- ; Drop TOS
-    8 # ( DP) PULU,           \ -- ; Restore DP
-    NEXT ;C
-
- CODE _Draw_VLc \ addr -- ;  addr = vector list in format: count, rel y, rel x, rel y, rel x, ...
-                \            current scaling factor is used
-    18 # ( X DP) PSHU,        \ -- addr ; Save X, DP
-    D0 # LDX,   X DPR TFR,    \ -- addr ; DP to D0
-    D X TFR,                  \ -- addr ; addr to X
-    Draw_VLc JSR,            \ Call Vectrex BIOS subroutine
-    6 # ( D) PULS,            \ -- addr ; Drop TOS
-    18 # ( X DP) PULU,        \ -- ; Restore X, DP
-    NEXT ;C
-
-
-\ \\\\\\\\\ WORKING
-
-CODE _Random \ -- n ; n is a random number between 0 and 255
-     6 # ( D) PSHS,   Random JSR,   CLRB,   A B EXG,   NEXT ;C
+\ Calibration/vector reset
 
 CODE _Wait_Recal \ -- ;
     1E # ( X DP D) PSHS,   Wait_Recal JSR,   1E # ( X DP D) PULS,   NEXT ;C
+
+CODE _Set_Refresh   NEXT ;C
 
 CODE _Recalibrate \ -- ;
     1E # ( X DP D) PSHS,
@@ -49,33 +16,80 @@ CODE _Recalibrate \ -- ;
     1E # ( X DP D) PULS,
     NEXT ;C
 
-CODE _Reset0Ref \ -- ; MOSTLY A COPY OF THE RECALIBRATE INTERFACE, SO SHOULD JUST WORK
+CODE _Reset0Ref_D0  NEXT ;C
+CODE _Check0Ref     NEXT ;C
+
+CODE _Reset0Ref \ -- ;
     1E # ( X DP D) PSHS,      \ Save Registers
     D0 # LDX,   X DPR TFR,    \ DP to D0
     Reset0Ref JSR,            \ Call Vectrex BIOS subroutine
     1E # ( X DP D) PULS,      \ Restore Registers
     NEXT ;C
 
-CODE _Reset_Pen \ -- ; MOSTLY A COPY OF THE RECALIBRATE INTERFACE, SO SHOULD JUST WORK
+CODE _Reset_Pen \ -- ;
     1E # ( X DP D) PSHS,      \ Save Registers
     D0 # LDX,   X DPR TFR,    \ DP to D0
     Reset_Pen JSR,            \ Call Vectrex BIOS subroutine
     1E # ( X DP D) PULS,      \ Restore Registers
     NEXT ;C
 
-CODE _Intensity_5F \ -- ;
-     8 # ( DP) PSHU,           \ -- ; Save DP
-     D0 # LDX,   X DPR TFR,    \ -- ; DP to D0
-     6 # ( D) PSHS,   Intensity_5F JSR,   6 # ( D) PULS,
-     8 # ( DP) PULU,           \ -- ; Restore DP
+CODE _Reset0Int NEXT ;C
+
+\ Day to Day
+
+\ CODE DPR!   B DPR TFR,   6 # ( D) PULS, NEXT ;C \ DPR -- ; Quicker to set DP directly
+CODE _DP_to_D0          NEXT ;C
+CODE _DP_to_C8          NEXT ;C
+CODE _Print_Ships_x     NEXT ;C
+CODE _Print_Ships       NEXT ;C
+CODE _Random_3          NEXT ;C
+
+CODE _Random \ -- n ; n is a random number between 0 and 255
+     6 # ( D) PSHS,   Random JSR,   CLRB,   A B EXG,   NEXT ;C
+
+CODE _Dec_3_Counters    NEXT ;C
+CODE _Dec_6_Counters    NEXT ;C
+CODE _Dec_Counters      NEXT ;C
+CODE _Bitmask_a         NEXT ;C
+
+\ Delay
+
+CODE _Delay_3           NEXT ;C
+CODE _Delay_2           NEXT ;C
+CODE _Delay_1           NEXT ;C
+CODE _Delay_0           NEXT ;C
+CODE _Delay_b           NEXT ;C
+CODE _Delay_RTS         NEXT ;C
+
+\ Drawing / Dot
+
+CODE _Dot_ix_b          NEXT ;C
+CODE _Dot_ix            NEXT ;C
+CODE _Dot_d             NEXT ;C
+
+CODE _Dot_here \ -- ;
+    1E # ( X DP D) PSHS,
+    D0 # LDX,   X DPR TFR,    \ -- ; DP to D0
+    Dot_here JSR,
+    1E # ( X DP D) PULS,
     NEXT ;C
 
-CODE _Intensity_7F \ -- ;
-     8 # ( DP) PSHU,           \ -- ; Save DP
-     D0 # LDX,   X DPR TFR,    \ -- ; DP to D0
-     6 # ( D) PSHS,   Intensity_7F JSR,   6 # ( D) PULS,
-     8 # ( DP) PULU,           \ -- ; Restore DP
+CODE _Dot_List \ dl_addr -- ; *** NOT TESTED ***
+    8 # ( DP) PSHU,           \ -- dl_addr ; Save DP
+    D0 # LDX,   X DPR TFR,    \ -- dl_addr ; DP to D0
+    D X TFR,                  \ -- dl_addr ; Load X with address of dot list
+    Dot_here JSR,             \ Call Vectrex BIOS subroutine
+    6 # ( D) PULS,            \ -- ; Drop TOS
+    8 # ( DP) PULU,           \ -- ; Restore DP
     NEXT ;C
+
+CODE _Dot_List          NEXT ;C
+CODE _Dot_List_Reset    NEXT ;C
+
+\ Drawing / String
+
+CODE _Print_Str_hwyx    NEXT ;C
+CODE _Print_Str_yx      NEXT ;C
 
 CODE _Print_Str_d \ x y c-addr -- ; Print single string to screen
     8 # ( DP) PSHU,           \ -- x y c-addr ; Save DP
@@ -90,32 +104,35 @@ CODE _Print_Str_d \ x y c-addr -- ; Print single string to screen
     8 # ( DP) PULU,           \ -- ; Restore DP
     NEXT ;C
 
-CODE _Moveto_d \ x y -- ;
-    8 # ( DP) PSHU,           \ -- x y ; Save DP
-    D0 # LDX,   X DPR TFR,    \ -- x y ; DP to D0
-    A B EXG,   S ,++ ADDD,    \ -- yx  ; Combine x and y
-    Moveto_d JSR,             \ Call Vectrex BIOS subroutine
-    6 # ( D) PULS,            \ -- ; Drop TOS
-    8 # ( DP) PULU,           \ -- ; Restore DP
+CODE _Print_List_hw     NEXT ;C
+CODE _Print_List        NEXT ;C
+CODE _Print_List_chk    NEXT ;C
+CODE _Print_Str         NEXT ;C
+
+\ drawing / Vector / Drawing and moving
+
+CODE _Mov_Draw_VLc_a    NEXT ;C
+CODE _Mov_Draw_VLc_b    NEXT ;C
+CODE _Mov_Draw_VLcs     NEXT ;C
+CODE _Mov_Draw_VL_ab    NEXT ;C
+CODE _Mov_Draw_VL_a     NEXT ;C
+CODE _Mov_Draw_VL       NEXT ;C
+CODE _Mov_Draw_VL_d     NEXT ;C
+
+\ Drawing / Vector / Drawing only
+
+\ *** NOT TESTED ***
+CODE _Draw_VLc \ addr -- ; addr = vector list in format: count, rel y, rel x, rel y, rel x, ...
+                \            current scaling factor is used
+    18 # ( X DP) PSHU,        \ -- addr ; Save X, DP
+    D0 # LDX,   X DPR TFR,    \ -- addr ; DP to D0
+    D X TFR,                  \ -- addr ; addr to X
+    Draw_VLc JSR,            \ Call Vectrex BIOS subroutine
+    6 # ( D) PULS,            \ -- addr ; Drop TOS
+    18 # ( X DP) PULU,        \ -- ; Restore X, DP
     NEXT ;C
 
-CODE _Moveto_d_7F \ x y -- ; Identical to _Moveto_d except for BIOS subroutine name
-    8 # ( DP) PSHU,           \ -- x y ; Save DP
-    D0 # LDX,   X DPR TFR,    \ -- x y ; DP to D0
-    A B EXG,   S ,++ ADDD,    \ -- yx  ; Combine x and y
-    Moveto_d_7F JSR,          \ Call Vectrex BIOS subroutine
-    6 # ( D) PULS,            \ -- ; Drop TOS
-    8 # ( DP) PULU,           \ -- ; Restore DP
-    NEXT ;C
-
-CODE _Draw_Line_d \ x y -- ;
-    8 # ( DP) PSHU,           \ -- x y c-addr ; Save DP
-    D0 # LDX,   X DPR TFR,    \ -- x y c-addr ; DP to D0
-    A B EXG,   S ,++ ADDD,    \ -- yx  ; Combine x and y
-    Draw_Line_d JSR,          \ Call Vectrex BIOS subroutine
-    6 # ( D) PULS,            \ -- ; Drop TOS
-    8 # ( DP) PULU,           \ -- ; Restore DP
-    NEXT ;C
+CODE _Draw_VL_b         NEXT ;C
 
 CODE _Draw_VLcs \ addr -- ;  addr = vector list in format: count, scale, rel y, rel x, rel y, rel x, ...
     18 # ( X DP) PSHU,        \ -- addr ; Save X, DP
@@ -136,12 +153,32 @@ CODE _Draw_VL_ab \ sf #v addr -- ;  addr = vector list in format: rel y, rel x, 
     18 # ( X DP) PULU,        \ --            ; Restore X, DP
     NEXT ;C
 
-CODE _Dot_here \ -- ;
-    1E # ( X DP D) PSHS,
-    D0 # LDX,   X DPR TFR,    \ -- ; DP to D0
-    Dot_here JSR,
-    1E # ( X DP D) PULS,
+CODE _Draw_VL_a         NEXT ;C
+CODE _Draw_VL           NEXT ;C
+
+CODE _Draw_Line_d \ x y -- ;
+    8 # ( DP) PSHU,           \ -- x y c-addr ; Save DP
+    D0 # LDX,   X DPR TFR,    \ -- x y c-addr ; DP to D0
+    A B EXG,   S ,++ ADDD,    \ -- yx  ; Combine x and y
+    Draw_Line_d JSR,          \ Call Vectrex BIOS subroutine
+    6 # ( D) PULS,            \ -- ; Drop TOS
+    8 # ( DP) PULU,           \ -- ; Restore DP
     NEXT ;C
+
+CODE _Draw_VLp_FF       NEXT ;C
+CODE _Draw_VLp_7F       NEXT ;C
+CODE _Draw_VLp_scale    NEXT ;C
+CODE _Draw_VLp_b        NEXT ;C
+CODE _Draw_VLp          NEXT ;C
+CODE _Draw_Pat_VL_a     NEXT ;C
+CODE _Draw_Pat_VL       NEXT ;C
+CODE _Draw_Pat_VL_d     NEXT ;C
+CODE _Draw_VL_mode      NEXT ;C
+CODE _Draw_Grid_VL      NEXT ;C
+
+\ Joystick handling
+
+CODE _Read_Btns_Mask    NEXT ;C
 
 CODE _Read_Btns \ -- b ; Button Transition State (Same as $C811)
     6 # ( D) PSHS,
@@ -152,9 +189,161 @@ CODE _Read_Btns \ -- b ; Button Transition State (Same as $C811)
     18 # ( X DP) PULS,
     NEXT ;C
 
+CODE _Joy_Analog        NEXT ;C
+
 CODE _Joy_Digital \ -- ;
     1E # ( X DP D) PSHS,
     D0 # LDX,   X DPR TFR,    \ -- ; DP to D0
     Joy_Digital JSR,
     1E # ( X DP D) PULS,
     NEXT ;C
+
+\ Mathematical
+
+CODE _Abs_a_b           NEXT ;C
+CODE _Abs_b             NEXT ;C
+
+\ Memory Management / Memory clear
+
+CODE _Clear_x_b         NEXT ;C
+CODE _Clear_C8_RAM      NEXT ;C
+CODE _Clear_x_256       NEXT ;C
+CODE _Clear_x_d         NEXT ;C
+
+\ Memory management / Memory copy
+
+CODE _Move_Mem_a_1      NEXT ;C
+CODE _Move_Mem_a        NEXT ;C
+
+\ Memory Management / Memory fill
+
+CODE _Clear_x_b_80      NEXT ;C
+CODE _Clear_x_b_a       NEXT ;C
+
+\ Player option
+
+CODE _Select_Game       NEXT ;C
+CODE _Display_Option    NEXT ;C
+
+\ Reset and initialization
+
+CODE _Cold_Start Cold_Start JMP, ;C \ -- ; Jump here to restart the Vectrex and re-initialize the OS.
+CODE _Warm_Start Warm_Start JMP, ;C \ -- ; Jump here to restart the Vectrex without re-initializing the OS.
+
+CODE _Init_VIA          NEXT ;C
+CODE _Init_OS_RAM       NEXT ;C
+CODE _Init_OS           NEXT ;C
+
+\ Score
+
+CODE _Clear_Score       NEXT ;C
+CODE _Add_Score_a       NEXT ;C
+CODE _Add_Score_d       NEXT ;C
+CODE _Strip_Zeros       NEXT ;C
+CODE _Compare_Score     NEXT ;C
+CODE _New_High_Score    NEXT ;C
+
+\ Sound
+
+CODE _Sound_Byte        NEXT ;C
+CODE _Sound_Byte_x      NEXT ;C
+CODE _Sound_Byte_raw    NEXT ;C
+CODE _Clear_Sound       NEXT ;C
+CODE _Sound_Bytes       NEXT ;C
+CODE _Sound_Bytes_x     NEXT ;C
+CODE _Do_Sound          NEXT ;C
+CODE _Do_Sound_x        NEXT ;C
+CODE _Init_Music_Buf    NEXT ;C
+CODE _Init_Music_chk    NEXT ;C
+CODE _Init_Music        NEXT ;C
+CODE _Init_Music_dft    NEXT ;C
+CODE _Explosion_Snd     NEXT ;C
+
+\ Vector beam positioning
+
+CODE _Moveto_x_7F       NEXT ;C
+
+CODE _Moveto_d_7F \ x y -- ;
+    8 # ( DP) PSHU,           \ -- x y ; Save DP
+    D0 # LDX,   X DPR TFR,    \ -- x y ; DP to D0
+    A B EXG,   S ,++ ADDD,    \ -- yx  ; Combine x and y
+    Moveto_d_7F JSR,          \ Call Vectrex BIOS subroutine
+    6 # ( D) PULS,            \ -- ; Drop TOS
+    8 # ( DP) PULU,           \ -- ; Restore DP
+    NEXT ;C
+
+CODE _Moveto_ix_FF      NEXT ;C
+CODE _Moveto_ix_7F      NEXT ;C
+CODE _Moveto_ix_b       NEXT ;C
+CODE _Moveto_ix         NEXT ;C
+
+CODE _Moveto_d \ x y -- ;
+    8 # ( DP) PSHU,           \ -- x y ; Save DP
+    D0 # LDX,   X DPR TFR,    \ -- x y ; DP to D0
+    A B EXG,   S ,++ ADDD,    \ -- yx  ; Combine x and y
+    Moveto_d JSR,             \ Call Vectrex BIOS subroutine
+    6 # ( D) PULS,            \ -- ; Drop TOS
+    8 # ( DP) PULU,           \ -- ; Restore DP
+    NEXT ;C
+
+\ Vector brightness
+
+CODE _Intensity_1F \ -- ;
+     8 # ( DP) PSHU,           \ -- ; Save DP
+     D0 # LDX,   X DPR TFR,    \ -- ; DP to D0
+     6 # ( D) PSHS,   Intensity_1F JSR,   6 # ( D) PULS,
+     8 # ( DP) PULU,           \ -- ; Restore DP
+    NEXT ;C
+
+CODE _Intensity_3F \ -- ;
+     8 # ( DP) PSHU,           \ -- ; Save DP
+     D0 # LDX,   X DPR TFR,    \ -- ; DP to D0
+     6 # ( D) PSHS,   Intensity_3F JSR,   6 # ( D) PULS,
+     8 # ( DP) PULU,           \ -- ; Restore DP
+    NEXT ;C
+
+CODE _Intensity_5F \ -- ;
+     8 # ( DP) PSHU,           \ -- ; Save DP
+     D0 # LDX,   X DPR TFR,    \ -- ; DP to D0
+     6 # ( D) PSHS,   Intensity_5F JSR,   6 # ( D) PULS,
+     8 # ( DP) PULU,           \ -- ; Restore DP
+    NEXT ;C
+
+CODE _Intensity_7F \ -- ;
+     8 # ( DP) PSHU,           \ -- ; Save DP
+     D0 # LDX,   X DPR TFR,    \ -- ; DP to D0
+     6 # ( D) PSHS,   Intensity_7F JSR,   6 # ( D) PULS,
+     8 # ( DP) PULU,           \ -- ; Restore DP
+    NEXT ;C
+
+CODE _Intensity_a \ n -- ;
+    8 # ( DP) PSHU,           \ -- TOS ; Save DP
+    D0 # LDX,   X DPR TFR,    \ -- TOS ; DP to D0
+    Intensity_a JSR,
+    6 # ( D) PULS,            \ -- ; Drop TOS
+    8 # ( DP) PULU,           \ -- ; Restore DP
+    NEXT ;C
+
+\ Vector object handling / Object collision detection
+
+CODE _Obj_Will_Hit_u    NEXT ;C
+CODE _Obj_Will_Hit      NEXT ;C
+CODE _Obj_Hit           NEXT ;C
+
+\ Vector object handling / Rotating
+
+CODE _Rise_Run_Angle    NEXT ;C
+CODE _Get_Rise_Idx      NEXT ;C
+CODE _Get_Run_Idx       NEXT ;C
+CODE _Rise_Run_Idx      NEXT ;C
+CODE _Rise_Run_X        NEXT ;C
+CODE _Rise_Run_Y        NEXT ;C
+CODE _Rise_Run_Len      NEXT ;C
+CODE _Rot_VL_ab         NEXT ;C
+CODE _Rot_VL            NEXT ;C
+CODE _Rot_VL_Mode       NEXT ;C
+CODE _Rot_VL_M_dft      NEXT ;C
+CODE _Xform_Run_a       NEXT ;C
+CODE _Xform_Run         NEXT ;C
+CODE _Xform_Rise_a      NEXT ;C
+CODE _Xform_Rise        NEXT ;C
--- VecForth/Vectrex/bios_api_tests.fs
+++ VecForth/Vectrex/bios_api_tests.fs
@@ -2,30 +2,34 @@
 
 HEX
 
-: DT \ -- ; Display turtle using vector list
+: KEY? 0 ;
+: KEY 1 ;
+: CR ;
+: EMIT DROP ;
+: U. DROP ;
+: .S ;
+
+HERE EQU HELLO-WORLD-STRING
+S" HELLO WORLD" 80 C,
+
+
+: DT \ -- ; Display turtles using vector list with two different BIOS calls
   0
   BEGIN
     1+ DUP U.
     _Wait_Recal
     _Intensity_7F
-    0 0 _Moveto_d
-    20 17 TURTLE _Draw_VL_ab \ scale factor and count of vectors
+
+    -1f 1f _Moveto_d
+    20 17 TURTLE _Draw_VL_ab    \ scale_factor count_of_vectors VL_addr
+
+    0 -7f _Moveto_d
+    0 -7f _Moveto_d
+    TURTLEcs _Draw_VLcs         \ draw vector list using BIOS call
+
     KEY?
   UNTIL
   DROP
-;
-
-: DTcs  \ -- ; Display turtle using vector list with count and scale
-  0                     \ initial frame counter
-  BEGIN
-    1+ DUP U.           \ display frame counter on the terminal
-    _Wait_Recal         \ BIOS recalibration
-    _Intensity_7F       \ set beam intensity
-    0 0 _Moveto_d       \ move beam to origin
-    TURTLEcs _Draw_VLcs \ draw vector list using BIOS call
-    KEY?                \ was a key pressed?
-  UNTIL
-  DROP                  \ drop frame counter
 ;
 
 : DG \ -- ; Draw grid line by line
@@ -36,8 +40,8 @@ HEX
     _Wait_Recal         \
     _Intensity_7F       \
 
-    \ start by moving beam to bottom left corner of grid
     0 Vec_Misc_Count C! \ Vec_Misc_Count must be set to 0 to draw one vector
+    \ start by moving beam to bottom left corner of grid, then zig-zag the lines
     -3F -3F _Moveto_d   0 7F _Draw_Line_d
      20  80 _Moveto_d   0 7F _Draw_Line_d
      20  80 _Moveto_d   0 7F _Draw_Line_d
@@ -54,14 +58,7 @@ HEX
   DROP
 ;
 
-: DG2 \ -- ; Dram grid l by l, but 2nd 5 lines drawn from opposite origin
-  FF VIA_t1_cnt_lo C!   \ set scaling factor
-  0                     \ initial frame counter
-  BEGIN
-    1 + DUP U.          \ Send a frame counter to display on the terminal
-    _Wait_Recal         \
-    _Intensity_7F       \
-
+: (dg2) \ -- ;
     0 Vec_Misc_Count C! \ Vec_Misc_Count must be set to 0 to draw one vector
     _Recalibrate   -3F -3F _Moveto_d   0  7F _Draw_Line_d
     _Recalibrate   -1F -3F _Moveto_d   0  7F _Draw_Line_d
@@ -73,13 +70,22 @@ HEX
     _Recalibrate    3F   0 _Moveto_d   -7F 0 _Draw_Line_d
     _Recalibrate    3F -1F _Moveto_d   -7F 0 _Draw_Line_d
     _Recalibrate    3F -3F _Moveto_d   -7F 0 _Draw_Line_d
+;
 
+: DG2 \ -- ; Draw grid line by line, but no zig zag, more optimal drawing
+  FF VIA_t1_cnt_lo C!   \ set scaling factor
+  0                     \ initial frame counter
+  BEGIN
+    1 + DUP U.          \ Send a frame counter to display on the terminal
+    _Wait_Recal         \
+    _Intensity_7F       \
+    (dg2)
     KEY?                \ terminal key press exits loop
   UNTIL
   DROP
 ;
 
-: DG3 \ -- ;
+: DG3 \ -- ; Draw grid with lops, no hardcoding of co-ordinates
   FF VIA_t1_cnt_lo C! \ set scaling factor
   0
   BEGIN
@@ -110,10 +116,8 @@ HEX
   DROP
 ;
 
-HERE EQU HELLO-WORLD-STRING
-S" HELLO WORLD" 80 C,
-
-: HELLO \ -- ;
+: HELLO \ -- ; Hello World, to vector display, to interactive
+             \ terminal and VecFever LED
   BEGIN
      _Wait_Recal
      _Intensity_5F
@@ -129,3 +133,26 @@ S" HELLO WORLD" 80 C,
      KEY 1B =
    UNTIL
 ;
+
+: box \ len_x len_y start_x start_y -- ;
+   _Reset0Ref
+   _Moveto_d  \ -- lx ly ;
+   dup 0         _Draw_Line_d
+   over 0 swap   _Draw_Line_d
+   negate 0      _Draw_Line_d
+   negate 0 swap _Draw_Line_d
+;
+
+: intensity \ -- ;
+  BEGIN
+    _Wait_Recal
+\     _Intensity_3F (dg2)
+\     _Intensity_5F 50 50 -27 -28 box
+    80 0 do
+        I 100 * _Intensity_a
+        -44 i 30 - HELLO-WORLD-STRING 3 + _Print_Str_d
+    10 +loop
+    KEY?
+  UNTIL
+;
+
